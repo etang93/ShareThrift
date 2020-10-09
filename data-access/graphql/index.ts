0476288eb41a4102cc8731d6a8fcf121339b621d
etang93 <eddietang715@gmail.com>
2020-10-09 11:45:33 -0400

    initial commit

---
+++ data-access/graphql/index.ts
@@ -0,0 +1,56 @@
+let appInsights = require("applicationinsights");
+appInsights.setup().start();
+appInsights.defaultClient.commonProperties = {
+  environment: process.env.WEBSITE_HOSTNAME,
+  functionArea: "graphql",
+};
+let appInsightsClient = appInsights.defaultClient;
+
+import {
+  ApolloServerPlugin,
+  GraphQLRequestContext,
+  GraphQLRequestListener,
+} from "apollo-server-plugin-base";
+import { ApolloServer, gql } from "apollo-server-azure-functions";
+
+// referenced from https://jeffmagnusson.com/post/graphql-apollo-server-plugins-in-typescript
+const appInsightsPlugin = <ApolloServerPlugin & GraphQLRequestListener>{
+  // Fires whenever a GraphQL request is received from a client.
+  requestDidStart(
+    requestContext: GraphQLRequestContext
+  ): GraphQLRequestListener | void {
+    appInsightsClient.trackMetric({ name: "apollo-query", value: 1 });
+    return this;
+  },
+  // Fires for graph exceptoions
+  didEncounterErrors: function (requestContext: GraphQLRequestContext) {
+    appInsightsClient.trackMetric({ name: "apollo-error", value: 1 });
+    appInsightsClient.trackException({ exception: new Error("Apollo Error") });
+    appInsightsClient.trackException({
+      exception: { category: "Apollo Error", details: requestContext.errors },
+    });
+  },
+};
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  plugins: [appInsightsPlugin],
+});
+
+exports.graphqlHandler = server.createHandler();
